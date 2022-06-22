8e69d0eeb7303f7f4b67073976347c7b3205c535
coolness10101 <aaron@missionarymall.org>
2022-06-22 13:23:22 -0600

    Did stuff

--- Game/Casting/Color.cs
+++ Game/Casting/Color.cs
@@ -4,6 +4,19 @@ namespace Greed_Game.Game.Casting
 {
     public class Color
     {
-        
+        public byte r { get; set; }
+        public byte g { get; set; }
+        public byte b { get; set; }
+        public byte a { get; set; }
+        public Color(byte r,byte g,byte b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+        public Raylib_cs.Color toRaylibColor()
+        {
+            return new Raylib_cs.Color(this.r, this.g, this.b, this.a);
+        }
     }
 }
